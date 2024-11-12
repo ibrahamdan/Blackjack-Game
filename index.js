4664a3e7f949f73b1b325bc91da63a1afe108886
ibra <ibraheemhamdan02@gmail.com>
2024-11-12 04:07:19 +0300

    improved functionality and game logic

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
+import { Card } from './card.js'
+
+
+
+document.getElementById('start-btn').addEventListener('click', startGame);
+document.getElementById('draw-btn').addEventListener('click', drawCard);
+
+
+let suits = ["Spades", "Diamonds", "Clubs", "Hearts"]
+let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+
+let deck = []
+
+for (let i = 0; i < suits.length; i++) {
+    for (let k = 0; k < values.length; k++) {
+        const card = new Card(suits[i], values[k])
+        deck.push(card)
+    }
+}
+
+let sum = 0
+let message = ""
+let gotBJ = false
 let isAlive = true
+let cards = []
 let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.querySelector("#cards-el")
@@ -6,8 +30,11 @@ let startBtn = document.querySelector("#start-btn")
 let firstRound = true
 
 function getRandomCard() {
-    let card = Math.floor(Math.random() * 10) + 1
-    return card
+    let cardPos = Math.floor(Math.random() * deck.length) + 1
+    let randomCard = deck[cardPos]
+    deck.splice(deck.indexOf(randomCard), 1)
+    console.log("generated rand card")
+    return randomCard.value
 }
 
 function displaySum() {
@@ -17,7 +44,22 @@ function displaySum() {
 function calculateSum() {
     sum = 0
     for (let i = 0; i < cards.length; i++) {
-        sum = sum + cards[i]
+        if (cards[i] == "J" || cards[i] == "Q" || cards[i] == "K" ) {
+            sum = sum + 10
+        }
+        else if (cards[i] == "A") {
+            let tempSum = sum
+            tempSum = tempSum + 11
+            if (tempSum > 21) {
+                sum = sum + 1
+            }
+            else {
+                sum = tempSum
+            }
+        }
+        else {
+            sum = sum + cards[i]
+        }
     }
     displaySum()
     updateMessage()
@@ -27,9 +69,7 @@ function calculateSum() {
 function displayCards() {
     for (let i = 0; i < cards.length; i++) {
         cardsEl.innerHTML = cardsEl.innerHTML + cards[i] + " "
-        console.log("stuck")
     }
-    console.log("displayed cards")
 }
 
 function updateMessage() {
@@ -51,10 +91,6 @@ function updateMessage() {
 function startGame() {
     startBtn.innerHTML = "New Game"
     cards = [getRandomCard(), getRandomCard()]
-    let sum = 0
-    let message = ""
-    let gotBJ = false
-    let isAlive = true
     sum = calculateSum()
     displayCards()
     if (!firstRound) {
